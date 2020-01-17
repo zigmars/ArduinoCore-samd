efca40a9f19a1643665aaf9b92626c0c5312cf42
Zigmārs Rupenheits <zigmarrs@gmail.com>
2020-01-17 14:28:09 +0200

    Configured pins and USB for RV1G MiniSumo 2018 Changed: 	USB device string; 	Serial to USB-CDC only (no UART); 	to correct LED pins.

--- bootloaders/zero/board_definitions_rv1g_minisumo_2018.h
+++ bootloaders/zero/board_definitions_rv1g_minisumo_2018.h
@@ -23,11 +23,13 @@
 /*
  * USB device definitions
  */
-#define STRING_PRODUCT "Arduino Zero"
+#define STRING_PRODUCT "RV1G MiniSumo 2018"
 #define USB_VID_HIGH   0x23
 #define USB_VID_LOW    0x41
 #define USB_PID_HIGH   0x00
 #define USB_PID_LOW    0x4D
+
+#define SAM_BA_USBCDC_ONLY
 
 /*
  * If BOOT_DOUBLE_TAP_ADDRESS is defined the bootloader is started by
@@ -49,8 +51,8 @@
 #define BOOT_USART_BUS_CLOCK_INDEX        PM_APBCMASK_SERCOM0
 #define BOOT_USART_PER_CLOCK_INDEX        GCLK_CLKCTRL_ID_SERCOM0_CORE_Val
 #define BOOT_USART_PAD_SETTINGS           UART_RX_PAD3_TX_PAD2
-#define BOOT_USART_PAD3                   PINMUX_PA11C_SERCOM0_PAD3
-#define BOOT_USART_PAD2                   PINMUX_PA10C_SERCOM0_PAD2
+#define BOOT_USART_PAD3                   PINMUX_UNUSED
+#define BOOT_USART_PAD2                   PINMUX_UNUSED
 #define BOOT_USART_PAD1                   PINMUX_UNUSED
 #define BOOT_USART_PAD0                   PINMUX_UNUSED
 
@@ -72,10 +74,10 @@
 #define BOARD_LED_PORT                    (0)
 #define BOARD_LED_PIN                     (17)
 
-#define BOARD_LEDRX_PORT                  (1)
-#define BOARD_LEDRX_PIN                   (3)
+#define BOARD_LEDRX_PORT                  (0)
+#define BOARD_LEDRX_PIN                   (2)
 
 #define BOARD_LEDTX_PORT                  (0)
-#define BOARD_LEDTX_PIN                   (27)
+#define BOARD_LEDTX_PIN                   (3)
 
 #endif // _BOARD_DEFINITIONS_H_
